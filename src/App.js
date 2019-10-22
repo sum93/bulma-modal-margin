fbba63181cfe5bc38d08487240eafabcbb79d3ab
Mate Jenei <mate_jenei@epam.com>
2019-10-22 10:52:48 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bulma/bulma.sass'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const modalContentStyle = {
+  padding: '40px 20px',
+  margin: '0 10px',
+  borderRadius: '10px',
+  backgroundColor: 'white',
+  color: 'gray'
 }
 
-export default App;
+const modalCardStyle = {
+  margin: '0 10px',
+  color: 'gray'
+}
+
+const App = () => {
+  const [active, setActive] = React.useState(false)
+  const [cardModal, setCardModal] = React.useState(false)
+
+  const toggleModal = () => { setCardModal(cardModal => !cardModal) }
+
+  return (
+    <section class="hero is-fullheight is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <div class={`modal${active ? ' is-active' : ''}`}>
+            <div class="modal-background"></div>
+            {cardModal ? (
+                <div class="modal-card">
+                  <div style={modalCardStyle}>
+                    <header class="modal-card-head">
+                      <p class="modal-card-title">Modal title</p>
+                      <button class="delete" onClick={() => setActive(false)} aria-label="close"></button>
+                    </header>
+                    <section class="modal-card-body">
+                      This is an example modal
+                    </section>
+                    <footer class="modal-card-foot">
+                      <button class="button is-success">Save changes</button>
+                      <button class="button" onClick={() => setActive(false)}>Cancel</button>
+                    </footer>
+                  </div>
+                </div>
+              ) : (
+                <React.Fragment>
+                  <div class="modal-content">
+                    <div style={modalContentStyle}>
+                      This is an example modal
+                    </div>
+                  </div>
+                  <button
+                    class="modal-close is-large"
+                    aria-label="close"
+                    onClick={() => setActive(false)}
+                  />
+                </React.Fragment>
+              )}
+          </div>
+          <h1 class="title">
+            Bulma Modal
+          </h1>
+          <h2 class="subtitle">
+            This is a demo of bulma modal
+          </h2>
+          <div className="buttons">
+            <button
+              class="button is-outlined is-medium"
+              onClick={toggleModal}
+            >
+              Toggle Modal
+            </button>
+            <button
+              class="button is-outlined is-medium"
+              onClick={() => setActive(true)}
+            >
+              Open Modal
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default App
